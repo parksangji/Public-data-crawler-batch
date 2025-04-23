959ecbf971db39bfa8316adaf6c2deea573fc255
sangji.park <parksangji1109@gmail.com>
2025-04-23 15:06:46 +0900

    first commit!

---
+++ data-crawler/src/main/java/com/example/datacrawler/DataCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.datacrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataCrawlerApplication.class, args);
+    }
+
+}
---
+++ data-crawler/src/test/java/com/example/datacrawler/DataCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datacrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
